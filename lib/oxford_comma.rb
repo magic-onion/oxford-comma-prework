c88b351748504bfec479f6749ff92a6a0f1850c4
Alexander Truong <alexander.m.truong@gmail.com>
2018-09-27 03:02:27 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,13 @@
 def oxford_comma(array)
-  last_thing = array.pop
-  last_thing
-  string_thing = array.join(", ")
-  string_thing
-  string_thing + ", and " + last_thing
-  
+  if array.length == 1 
+    array[0].to_s 
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    last_thing = array.pop
+    last_thing
+    string_thing = array.join(", ")
+    string_thing
+    string_thing + ", and " + last_thing
+  end
 end
